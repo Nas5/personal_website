31e9e74c62430996c251f5a4433db1177c05d910
David Nasonov <112696778+Nas5@users.noreply.github.com>
2025-07-23 12:27:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,12 @@
 # exit on error
 set -o errexit
 
-mkdir -p static
 # Create and activate virtual environment
 echo "==> Installing requirements"
-pip install --upgrade pip
 pip install -r requirements.txt
-
-
-echo "==> Checking installed packages"
-pip list | grep gunicorn
-
 
 # Upgrade pip and install requirements
 pip install cloudinary django-cloudinary-storage pillow
 # Django commands
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
