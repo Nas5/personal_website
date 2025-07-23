27a8a4e95fbb15619cce1a3e2bd785fa4724548c
David Nasonov <112696778+Nas5@users.noreply.github.com>
2025-07-23 11:06:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,10 +15,6 @@ pip list | grep gunicorn
 
 # Upgrade pip and install requirements
 pip install pillow
-pip install gunicorn
-pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
-
 # Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
