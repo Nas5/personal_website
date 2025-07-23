9e1f69a6064f8d73a24451fb0bd055bca11e5de3
David Nasonov <112696778+Nas5@users.noreply.github.com>
2025-07-23 11:06:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ set -o errexit
 
 mkdir -p static
 # Create and activate virtual environment
-python -m venv venv
-source venv/bin/activate
+echo "==> Installing requirements"
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
+echo "==> Checking installed packages"
+pip list | grep gunicorn
+
 
 # Upgrade pip and install requirements
 pip install pillow
