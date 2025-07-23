71e804b2423b0ce66a13a50bfd564920f02c96e0
David Nasonov <112696778+Nas5@users.noreply.github.com>
2025-07-23 13:10:25 +0100

    Update settings.py

--- bio_website/settings.py
+++ bio_website/settings.py
@@ -101,6 +101,14 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
+# Configure Cloudinary
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True  # Use HTTPS URLs
+)
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
